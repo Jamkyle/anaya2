28b1ad09a30dbdbb75e33d9e5ef1f21a73a29cdc
Aro Rahari <Aro@MacBook-Pro-de-Aro.local>
2016-01-22 00:56:47 +0100

    fix draw

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', () => {
-  gulp.src('./sass/**/*.{scss,sass}')
+  gulp.src('./sass/bubble.scss')
   .pipe(sass())
-  .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'));
 });
+
+gulp.task('default', ['sass'])
